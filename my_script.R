bff07aa02b17e18f82376f3e6e4a03ed25fd67e2
Brad Hill <bhill00@ucsb.edu>
2023-04-26 18:45:50 +0000

    test

--- my_script.R
+++ my_script.R
@@ -9,6 +9,7 @@ cats <- data.frame(coat = c("calico", "black", "tabby"),
 #write cats to new file called feline-data.csv
 write.csv(x = cats, file = "data/feline-data.csv", row.names = FALSE)
 #test
+#
 #look at specific columns
 cats$weight
 cats$coat
